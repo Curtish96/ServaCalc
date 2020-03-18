70e758c8d4bf08d29da2e80e9870b855dfe0c203
Curtis Hawkins <33757295+Curtish96@users.noreply.github.com>
2020-03-18 15:58:59 +0000

    update

--- servacalc.py
+++ servacalc.py
@@ -1,2 +1,10 @@
 ## ServaCalc, a simple calculation tool used to determine the cost of running a physical server over the course of a month by inputting specifications.
 
+try:
+    towerCost = float(input("What is the cost of your tower unit?"))
+
+except ValueError:
+    print("Please only enter digits.")
+
+else:
+    ## To be continued
